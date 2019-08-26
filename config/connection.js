b4d2064d9024e30521f1221ac53353e46584b780
Anum Asif <unum.asif@gmail.com>
2019-08-25 21:13:49 -0700

    readme

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,5 @@
 var mysql = require('mysql');
 
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "root",
-//     database: "burgers_db"
-//   });
-
-  
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
